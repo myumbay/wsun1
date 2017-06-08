68e6a09803f07c5066976ffb57db1386b1de6586
myumbay <carolyumbay@gmail.com>
2017-06-07 22:04:54 -0500

    categoria

--- src/WsunBundle/Form/CategoriaType.php
+++ src/WsunBundle/Form/CategoriaType.php
@@ -2,8 +2,12 @@
 
 namespace WsunBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -14,12 +18,30 @@ class CategoriaType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+            $validador=function (FormEvent $event) {
+                $product = $event->getData();
+                $form = $event->getForm();
+
+        };
+
         $builder
                 ->add('nombreCat',  TextType::class,array('label'=>'Nombre Categoria:'))
                 ->add('padreId' ,  \Symfony\Component\Form\Extension\Core\Type\HiddenType::class)//, array("attr"=>array("class"=>"form form-control")))
-                ->add('estado',  CheckboxType::class,array('label'=>'Estado:'))
-                ->add('padre');
-                
+                ->add('estado',  CheckboxType::class ,array('label'=>'Activar Categoria Principal:','required' => false,'attr'=>array('Escoger Categoria')))
+
+                 ->add('padre', EntityType::class, array(
+                    'class' => 'WsunBundle:Categoria',
+                    'query_builder' => function(EntityRepository $er) {
+                        return $er->createQueryBuilder('c')
+                            ->andWhere('c.padreId is NULL')
+                            //->andWhere('c.padreId =c.id')
+                            ->orderBy('c.nombreCat', 'ASC');
+                    },
+                ));
+                $builder->addEventListener(FormEvents::PRE_SET_DATA,$validador);
+               // ->add('padre');
+
                 
         
     }
